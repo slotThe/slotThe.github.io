70741d9943224dbadfc31864b9af9c8e638cf3cb
Tony Zorman <soliditsallgood@mailbox.org>
2022-07-28 11:08:26 +0200

    site.hs: Cleanup

--- site.hs
+++ site.hs
@@ -3,17 +3,17 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 
 import Data.Text qualified as T
+import GHC.Exts  qualified as Ext
 
 import Control.Arrow
 import Data.Monoid
 import Data.Text (Text)
 import Hakyll
 import Skylighting.Types hiding (Context)
-import Text.HTML.TagSoup qualified as TS
+import Text.HTML.TagSoup (Tag (TagClose, TagOpen))
 import Text.Pandoc.Highlighting
 import Text.Pandoc.Options
 import Text.Pandoc.Templates (compileTemplate)
-import GHC.Exts qualified as Ext
 
 main :: IO ()
 main = hakyllWith config do
@@ -29,27 +29,30 @@ main = hakyllWith config do
     route   idRoute
     compile copyFileCompiler
 
-  match (fromList ["about.md", "impressum.md"]) do
-    route   $ setExtension "html"
-    compile $ pandocCompiler      -- no toc
-          >>= loadAndApplyTemplate
-                "templates/default.html"
-                (boolField "noindex" (pure True) <> defaultContext)
-          >>= relativizeUrls
+  match "index.html" do
+    route idRoute
+    compile do
+      posts <- recentFirst =<< loadAllSnapshots allPosts "post-teaser"
+      let teaserCtx = teaserField "teaser" "post-teaser" <> postCtx
+          indexCtx  = listField "posts" teaserCtx (return posts) <> defaultContext
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= relativizeUrls
 
   match (fromList ["research.md", "free-software.md"]) do
     route $ setExtension "html"
-    compile $ myPandocCompiler   -- with toc
+    compile $ myPandocCompiler
           >>= loadAndApplyTemplate "templates/default.html" defaultContext
           >>= relativizeUrls
 
-  match allPosts do
-    route $ setExtension "html"
-    compile $ myPandocCompiler
-          >>= mkTeaserSnapshot
-          >>= loadAndApplyTemplate "templates/post.html"    postCtx
-          >>= saveSnapshot "post-content"
-          >>= loadAndApplyTemplate "templates/default.html" defaultContext
+  match (fromList ["about.md", "impressum.md"]) do
+    route   $ setExtension "html"
+    compile $ pandocCompilerNoToc
+          >>= loadAndApplyTemplate
+                "templates/default.html"
+                (  boolField "noindex" (pure True)  -- pls no index
+                <> defaultContext )
           >>= relativizeUrls
 
   create ["posts.html"] do
@@ -64,6 +67,15 @@ main = hakyllWith config do
           >>= loadAndApplyTemplate "templates/default.html"   allPostsCtx
           >>= relativizeUrls
 
+  match allPosts do
+    route $ setExtension "html"
+    compile $ myPandocCompiler
+          >>= mkTeaserSnapshot             -- For the previews on the main page.
+          >>= loadAndApplyTemplate "templates/post.html"    postCtx
+          >>= saveSnapshot "post-content"  -- For atom feed.
+          >>= loadAndApplyTemplate "templates/default.html" defaultContext
+          >>= relativizeUrls
+
   create ["css/syntax.css"] do  -- Syntax highlighting; see below.
     route idRoute
     compile . makeItem $ styleToCss highlightTheme
@@ -73,17 +85,6 @@ main = hakyllWith config do
     compile do
       lastPosts <- recentFirst =<< loadAllSnapshots allPosts "post-content"
       renderAtom feedConfig (postCtx <> bodyField "description") lastPosts
-
-  match "index.html" do
-    route idRoute
-    compile do
-      posts <- recentFirst =<< loadAllSnapshots allPosts "post-teaser"
-      let teaserCtx = teaserField "teaser" "post-teaser" <> postCtx
-          indexCtx  = listField "posts" teaserCtx (return posts) <> defaultContext
-      getResourceBody
-        >>= applyAsTemplate indexCtx
-        >>= loadAndApplyTemplate "templates/default.html" defaultContext
-        >>= relativizeUrls
 
   match "templates/*" $ compile templateBodyCompiler
 
@@ -106,11 +107,15 @@ feedConfig = FeedConfiguration
   , feedRoot        = "https://tony-zorman.com"
   }
 
+-- | Emphasise that the default pandoc compiler does not have a TOC.
+pandocCompilerNoToc :: Compiler (Item String)
+pandocCompilerNoToc = pandocCompiler
+
 -- | Pandoc compiler with syntax highlighting and a table of contents.
 --
 -- Sources:
---   <https://rebeccaskinner.net/posts/2021-01-31-hakyll-syntax-highlighting.html syntax highlighting>,
---   <https://svejcar.dev/posts/2019/11/27/table-of-contents-in-hakyll/ TOC>.
+--   + syntax highlighting: https://rebeccaskinner.net/posts/2021-01-31-hakyll-syntax-highlighting.html
+--   + TOC                : https://svejcar.dev/posts/2019/11/27/table-of-contents-in-hakyll/
 myPandocCompiler :: Compiler (Item String)
 myPandocCompiler = pandocCompilerWith defaultHakyllReaderOptions
                =<< myPandocWriterOptions
@@ -140,28 +145,29 @@ myPandocCompiler = pandocCompilerWith defaultHakyllReaderOptions
 suppressToc :: Item String -> Item String
 suppressToc = fmap (withTagList suppressor)
  where
-  suppressor :: [TS.Tag String] -> [TS.Tag String]
-  suppressor tags = pre ++ post
-   where (pre, (_, post)) = second (second (drop 1) . break (== TS.TagClose "div"))
-                          . break (== TS.TagOpen "div" [("id", "contents")])
-                          $ tags
+  suppressor :: [Tag String] -> [Tag String]
+  suppressor tags = pre <> post
+   where
+    (pre, (_, post)) = second (second (drop 1) . break (== TagClose "div"))
+                     . break (== TagOpen "div" [("id", "contents")])
+                     $ tags
 
 mkTeaserSnapshot ::  Item String -> Compiler (Item String)
 mkTeaserSnapshot item = item <$ saveSnapshot "post-teaser" (suppressToc item)
 
--- | In the spirit of the stimmung themes:
--- https://github.com/motform/stimmung-themes
+-- | In the spirit of the stimmung-themes:
+--       https://github.com/motform/stimmung-themes
 highlightTheme :: Style
 highlightTheme = monochrome
   { tokenStyles
       = Ext.fromList
-          [ (CommentTok, defStyle{ tokenColor  = color 0x505050
-                                 , tokenItalic = True
-                                 })
+          [ (CommentTok , defStyle{ tokenColor  = color 0x505050
+                                  , tokenItalic = True
+                                  })
           , (DataTypeTok, defStyle{ tokenBackground = color 0xf8edff })
-          , (StringTok, defStyle{ tokenBackground = color 0xf2f2f2 })
+          , (StringTok  , defStyle{ tokenBackground = color 0xf2f2f2 })
           , (OperatorTok, defStyle{ tokenBold = True })
-          , (OtherTok, defStyle{ tokenBold = True })
+          , (OtherTok   , defStyle{ tokenBold = True })
           ]
      <> tokenStyles monochrome
   }
