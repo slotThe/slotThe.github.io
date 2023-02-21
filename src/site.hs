ac5c74cc2709e17c01a886417f139a6f3e98f454
Tony Zorman <soliditsallgood@mailbox.org>
2023-02-21 19:41:17 +0100

    site: Automatically smallcaps API

--- src/site.hs
+++ src/site.hs
@@ -345,7 +345,7 @@ myPandocCompiler =
            $ replaceXMonad
            $ foldl' (replace T.toLower)
                     t
-                    ["HTML", "CSS", "GNU", "MELPA", "ELPA", "FLOSS", "AST", "KDE", "XML", "CLI", "QMK", "GHC", "PDF", "GMM", "QGS", "PSSL", "TODO", "EDSL", "DSL"]
+                    ["HTML", "CSS", "GNU", "MELPA", "ELPA", "FLOSS", "AST", "KDE", "XML", "CLI", "QMK", "GHC", "PDF", "GMM", "QGS", "PSSL", "TODO", "EDSL", "DSL", "API"]
     inline -> inline
    where
     replace :: (Text -> Text) -> Text -> Text -> Text
