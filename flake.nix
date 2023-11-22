ae5b6951a3668329e8705e3104e391eac6a65021
Tony Zorman <soliditsallgood@mailbox.org>
2023-11-22 11:09:16 +0100

    flake: Update
    + Remove flake-utils, as it is really not needed.
    + Add zlib to dependencies, so cabal build works.
    + Add haskell-language-server.
    + Add pandoc-sidenote as a local dependency to the flake.
    + Support for `nix build' and `nix run'.
    
    All in one ball of a commit :)

--- flake.nix
+++ flake.nix
@@ -1,29 +1,34 @@
 {
-  inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-    utils.url   = github:numtide/flake-utils;
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        overlay = final: prev: {
-          haskellPackages = prev.haskellPackages.override (old: {
-            overrides = final.haskell.lib.packageSourceOverrides {
-              site = ./.;
-            };
-          });
-        };
-        pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            (python311.withPackages (p: [ p.pygments ])) # Syntax highlighting
-            nodejs_20  # Offline LaTeX SVG creation
-            parallel   # Process LaTeX snippets in parallel
-          ];
-          inputsFrom  = [ pkgs.haskellPackages.site.env ];
-        };
-        }
-    );
+  outputs = { nixpkgs, ... }:
+    let system = "x86_64-linux";
+        pkgs   = nixpkgs.legacyPackages.${system};
+        hPkgs  = pkgs.haskellPackages.extend (self: super: {
+          site            = self.callCabal2nix "site" ./. { };
+          pandoc-sidenote = self.callCabal2nixWithOptions "pandoc-sidenote"
+            (builtins.fetchGit {
+              url = "https://github.com/slotThe/pandoc-sidenote";
+              ref = "feat/html-sidenotes";
+              rev = "402116d7039a801bd600c5155bfd52faac394258";
+            })
+            "-f html-sidenotes"
+            { };
+        });
+    in {
+      # nix build
+      packages.${system}.default = hPkgs.site;
+
+      # nix develop
+      devShells.${system}.default = hPkgs.shellFor {
+        packages          = p: [ p.site ];
+        nativeBuildInputs = [ hPkgs.haskell-language-server ];
+        buildInputs       = with pkgs; [
+          zlib
+          (python311.withPackages (p: [ p.pygments ])) # Syntax highlighting
+          nodejs_20  # Offline LaTeX SVG creation
+          parallel   # Process LaTeX snippets in parallel
+        ];
+      };
+    };
 }
