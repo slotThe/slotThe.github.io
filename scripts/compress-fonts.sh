ec3558a4f074f0f007e836be9f9c7bf69bd20c01
Tony Zorman <mail@tony-zorman.com>
2024-09-24 08:57:13 +0200

    fonts: Add more Umlauts

--- scripts/compress-fonts.sh
+++ scripts/compress-fonts.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #
 # Source: https://xeiaso.net/blog/iaso-fonts/
+#
+# Some Unicode symbols added.
 
 pyftsubset \
     $1 \
@@ -12,4 +14,5 @@ pyftsubset \
     --unicodes="U+0000-00A0,U+00A2-00A9,U+00AC-00AE,U+00B0-00B7,\
       U+00B9-00BA,U+00BC-00BE,U+00D7,U+00F7,U+2000-206F,U+2074,\
       U+20AC,U+2122,U+2190-21BB,U+2212,U+2215,U+F8FF,U+FEFF,\
-      U+FFFD,U+00E8,U+00E9,U+00F2,U+00F3"
+      U+FFFD,U+00E8,U+00E9,U+00F2,U+00F3,U+00E4,U+00C4,U+00D6,\
+      U+00DC,U+00F6,U+00FC"
